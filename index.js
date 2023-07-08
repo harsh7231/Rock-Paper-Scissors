bad989e2d8b4e284a7d84ae15072e7b7be2e95d2
Harsh Kumar rattewal <91414873+harsh7231@users.noreply.github.com>
2023-07-08 22:03:47 +0530

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ closeButton.addEventListener('click', () => {
 });
 function opennewpage() {
     window.location.href='won.html';
-    
+    updateScore();
 }
 let playerScore = localStorage.getItem('playerScore') || 0;
 let computerScore = localStorage.getItem('computerScore') || 0;
@@ -31,26 +31,26 @@ function play(playerChoice) {
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
-         document.getElementById('next-button').style.display = 'block';
         playerScore++;
+        document.getElementById('next-button').style.display = 'block';
         document.getElementById('win-area').style.display='block';
         if(playerChoice==='rock'){
-            document.getElementById('user-rock-win').style.display='flex';
+            document.getElementById('user-rock-win').style.display='block';
         }
         else if(playerChoice==='paper'){
-            document.getElementById('user-paper-win').style.display='flex';
+            document.getElementById('user-paper-win').style.display='block';
         }
         else{
-            document.getElementById('user-scissor-win').style.display='flex';
+            document.getElementById('user-scissor-win').style.display='block';
         }
         if(computerChoice==='rock'){
-            document.getElementById('cpu-rock-win').style.display='flex';
+            document.getElementById('cpu-rock-win').style.display='block';
         }
         else if(computerChoice==='paper'){
-            document.getElementById('cpu-paper-win').style.display='flex';
+            document.getElementById('cpu-paper-win').style.display='block';
         }
         else {
-            document.getElementById('cpu-scissor-win').style.display='flex';
+            document.getElementById('cpu-scissor-win').style.display='block';
         }
     
     } 
@@ -58,56 +58,53 @@ function play(playerChoice) {
     (computerChoice === 'paper' && playerChoice === 'rock') ||
     (computerChoice === 'scissors' && playerChoice === 'paper')) 
     {
-         document.getElementById('next-button').style.display='none';
         document.getElementById('lost-area').style.display='block';
         if(playerChoice==='rock'){
-            document.getElementById('user-rock-lost').style.display='flex';
+            document.getElementById('user-rock-lost').style.display='block';
         }
         else if(playerChoice==='paper'){
-            document.getElementById('user-paper-lost').style.display='flex';
+            document.getElementById('user-paper-lost').style.display='block';
         }
         else {
-            document.getElementById('user-scissor-lost').style.display='flex';
+            document.getElementById('user-scissor-lost').style.display='block';
         }
         if(computerChoice==='rock'){
-            document.getElementById('cpu-rock-lost').style.display='flex';
+            document.getElementById('cpu-rock-lost').style.display='block';
         }
         else if(computerChoice==='paper'){
-            document.getElementById('cpu-paper-lost').style.display='flex';
+            document.getElementById('cpu-paper-lost').style.display='block';
         }
         else {
-            document.getElementById('cpu-scissor-lost').style.display='flex';
+            document.getElementById('cpu-scissor-lost').style.display='block';
         }
-    
+        document.getElementById('next-button').style.display='none';
         computerScore++;
     }
     else { 
-         document.getElementById('next-button').style.display='none';
         if(playerChoice==='rock'){
-            document.getElementById('user-rock-tie').style.display='flex';
+            document.getElementById('user-rock-tie').style.display='block';
         }
         else if(playerChoice==='paper'){
-            document.getElementById('user-paper-tie').style.display='flex';
+            document.getElementById('user-paper-tie').style.display='block';
         }
         else {
-            document.getElementById('user-scissor-tie').style.display='flex';
+            document.getElementById('user-scissor-tie').style.display='block';
         }
         if(computerChoice==='rock'){
-            document.getElementById('cpu-rock-tie').style.display='flex';
+            document.getElementById('cpu-rock-tie').style.display='block';
         }
         else if(computerChoice==='paper'){
-            document.getElementById('cpu-paper-tie').style.display='flex';
+            document.getElementById('cpu-paper-tie').style.display='block';
         }
         else {
-            document.getElementById('cpu-scissor-tie').style.display='flex';
+            document.getElementById('cpu-scissor-tie').style.display='block';
         }
-    
-        document.getElementById('tie-area').style.display='flex';
+        document.getElementById('next-button').style.display='none';
+        document.getElementById('tie-area').style.display='block';
     }
     updateScore();
     localStorage.setItem('playerScore', playerScore);
     localStorage.setItem('computerScore', computerScore);
-   
     document.getElementById('game-area').style.display='none';
     
 }
